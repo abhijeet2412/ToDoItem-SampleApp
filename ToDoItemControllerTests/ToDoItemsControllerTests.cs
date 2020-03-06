1c0a6874001984007d40ab43864141b52b8d010a
Abhijeet Singh <abhijeet.singh@cloudesign.in>
2020-03-06 14:17:13 +0530

    First Sample Unit Test Added

---
+++ ToDoItemControllerTests/ToDoItemsControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using WebApplication3.Controllers;
+using Xunit;
+
+namespace ToDoItemControllerTests
+{
+    public class TodoItemsController_itemsController
+    {
+
+        // private readonly TodoItemsController _itemsController;
+
+        //public TodoItemsController_itemsController()
+        //{
+        //    _itemsController = new TodoItemsController();
+        //}
+
+        [Fact]
+        public void TestGetToDoItem()
+        {
+            var item = 1;
+            Assert.Equal(1, item);
+        }
+    }
+}
